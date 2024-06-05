45fdddaa625eb695d21de159f1e57c364f2f0644
Arbaan Qureshi S <102940891+arbaan-max@users.noreply.github.com>
2024-06-05 12:20:45 +0530

    modification

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ app.use(
 );
 app.use("/api/v1/admin", adminRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.get("/api/test", (req, res) => {
   res.send("Server Working");
